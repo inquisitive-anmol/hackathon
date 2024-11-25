76e9aa1dc13e2af59e50f21e60e8a0f51ebec7f1
anmol <anmolof10gms@gmail.com>
2024-11-25 13:15:21 +0530

    fixed package json bug type module

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react'
 import { Route, Routes } from "react-router-dom";
 import webFont from "webfontloader";
 import Header from './components/layout/Header';
+import Home from './components/Home/home';
+import Doctor from './components/Doctor/Doctor';
+import Contact from './components/contact/Contact';
 
 
 
@@ -23,7 +26,9 @@ useEffect(() => {
        <Header />
       <Routes>
 
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/doctor" element={<Doctor />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   )
