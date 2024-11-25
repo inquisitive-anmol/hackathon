94cf0e03060cb4afc1e616e05e512ee44fa75b30
anmol <anmolof10gms@gmail.com>
2024-11-25 11:48:06 +0530

    fixed package json bug type module

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Route, Routes } from "react-router-dom";
+import webFont from "webfontloader";
+import Header from './components/layout/Header';
+
+
+
 
 const App = () => {
+
+  
+useEffect(() => {
+  webFont.load({
+    google: {
+      families: ["Poppins", "Nunito", "Roboto"],
+    },
+  });
+
+
+}, []);
   return (
     <div>
-      App
+       <Header />
+      <Routes>
+
+        {/* <Route path="/" element={<Home />} /> */}
+      </Routes>
     </div>
   )
 }
 
 export default App
+
+
+
