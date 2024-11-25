92603eabc6f3cbd4766268943b30059bcdaed7f6
Vaibhav-Singh2 <mathura.vs@gmail.com>
2024-11-25 13:19:42 +0530

    fix

--- backend/app.js
+++ backend/app.js
@@ -7,30 +7,29 @@ const cors = require("cors");
 const bodyParsor = require("body-parser");
 const fileUpload = require("express-fileupload");
 
-
 const errorMiddleware = require("./middleware/error");
 
 // Confige
-dotenv.config({path: "config/config.env"});
-
+dotenv.config();
 
 const corsOptions = {
-    origin: process.env.BASE_FRONTEND_URL,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-File-Name'],
-    credentials: true
+  origin: process.env.BASE_FRONTEND_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "X-Requested-With",
+    "X-File-Name",
+  ],
+  credentials: true,
 };
-
-
 
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
-app.use(bodyParsor.urlencoded({extended: true}));
+app.use(bodyParsor.urlencoded({ extended: true }));
 app.use(fileUpload());
-
-
 
 // Route Imports
 const product = require("./routes/productRoute");
@@ -38,17 +37,10 @@ const user = require("./routes/userRoutes");
 const order = require("./routes/orderRoute");
 const payment = require("./routes/paymentRoute");
 
-
-
-
-
-
-
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
-
 
 // Middleware for Errors
 
